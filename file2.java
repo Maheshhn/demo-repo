f138bd177395a1499b14a8e97f7730c410ad5765
Mahesh H N <maheshhn@eximiusdesign.in>
2020-12-04 17:39:00 +0530

    test

---
+++ file2.java
@@ -0,0 +1,18 @@
+$Recycle.Bin/
+$SysReset/
+$WINDOWS.~BT/
+Documents and Settings@
+HashiCorp/
+hiberfil.sys
+Intel/
+OneDriveTemp/
+pagefile.sys
+PerfLogs/
+Program Files/
+Program Files (x86)/
+ProgramData/
+Recovery/
+swapfile.sys
+System Volume Information/
+Users/
+Windows/
